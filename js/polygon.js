aa25c7963298df1f999eacd75c4999fb1e4bed44
Raymond Zhou <somecrazyidiot@gmail.com>
2014-04-10 23:07:45 -0400

    Add Polygon centroid() method.

--- js/polygon.js
+++ js/polygon.js
@@ -31,7 +31,7 @@ var Polygon = (function() {
 
     ctx.moveTo( vertices[0], vertices[1] );
     for ( var i = 1, il = 0.5 * vertices.length; i < il; i++ ) {
-      ctx.lineTo( vertices[ 2 * i ], vertices[2 * i + 1 ] );
+      ctx.lineTo( vertices[ 2 * i ], vertices[ 2 * i + 1 ] );
     }
   };
 
@@ -105,6 +105,51 @@ var Polygon = (function() {
     return {
       x: x + this.x,
       y: y + this.y
+    };
+  };
+
+  /**
+   * Computes the centroid in local coordinates.
+   */
+  Polygon.prototype.centroid = function() {
+    var vertices = this.vertices;
+    var vertexCount = 0.5 * vertices.length;
+
+    // Centroid.
+    var x = 0,
+        y = 0;
+
+    var area = 0;
+    var x0 = 0,
+        y0 = 0;
+
+    var third = 1 / 3;
+
+    var triangleArea;
+    var x1, y1, x2, y2;
+    var dx0, dy0, dx1, dy1;
+    for ( var i = 0; i < vertexCount; i++ ) {
+      x1 = vertices[ 2 * i ];
+      y1 = vertices[ 2 * i + 1 ];
+      x2 = vertices[ 2 * ( ( i + 1 ) % vertexCount ) ];
+      y2 = vertices[ 2 * ( ( i + 1 ) % vertexCount ) + 1 ];
+
+      dx0 = x1 - x0;
+      dy0 = y1 - x0;
+      dx1 = x2 - x1;
+      dy1 = y2 - y1;
+
+      // Half the 2D 'cross product'.
+      triangleArea = 0.5 * ( dx0 * dy1 - dx1 * dy0 );
+
+      area += triangleArea;
+      x += triangleArea * third * ( x0 + x1 + x2 );
+      y += triangleArea * third * ( y0 + y1 + y2 );
+    }
+
+    return {
+      x: x / area,
+      y: y / area
     };
   };
 

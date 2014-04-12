fe4684f59b18c02dda50dd7457b3c03044262a70
Raymond Zhou <somecrazyidiot@gmail.com>
2014-04-12 01:49:31 -0400

    Replace some Polygon method implementations with Geometry methods.

--- js/polygon.js
+++ js/polygon.js
@@ -1,3 +1,4 @@
+/*globals Geometry*/
 /*exported Polygon*/
 var Polygon = (function() {
   'use strict';
@@ -24,15 +25,7 @@ var Polygon = (function() {
   };
 
   Polygon.prototype.drawPath = function( ctx ) {
-    var vertices = this.vertices;
-    if ( vertices.length < 2 ) {
-      return;
-    }
-
-    ctx.moveTo( vertices[0], vertices[1] );
-    for ( var i = 1, il = 0.5 * vertices.length; i < il; i++ ) {
-      ctx.lineTo( vertices[ 2 * i ], vertices[ 2 * i + 1 ] );
-    }
+    Geometry.drawPolygon( ctx, this.vertices );
   };
 
   Polygon.prototype.applyTransform = function( ctx ) {
@@ -112,45 +105,7 @@ var Polygon = (function() {
    * Computes the centroid in local coordinates.
    */
   Polygon.prototype.centroid = function() {
-    var vertices = this.vertices;
-    var vertexCount = 0.5 * vertices.length;
-
-    // Centroid.
-    var x = 0,
-        y = 0;
-
-    var area = 0;
-    var x0 = 0,
-        y0 = 0;
-
-    var third = 1 / 3;
-
-    var triangleArea;
-    var x1, y1, x2, y2;
-    var dx0, dy0, dx1, dy1;
-    for ( var i = 0; i < vertexCount; i++ ) {
-      x1 = vertices[ 2 * i ];
-      y1 = vertices[ 2 * i + 1 ];
-      x2 = vertices[ 2 * ( ( i + 1 ) % vertexCount ) ];
-      y2 = vertices[ 2 * ( ( i + 1 ) % vertexCount ) + 1 ];
-
-      dx0 = x1 - x0;
-      dy0 = y1 - x0;
-      dx1 = x2 - x1;
-      dy1 = y2 - y1;
-
-      // Half the 2D 'cross product'.
-      triangleArea = 0.5 * ( dx0 * dy1 - dx1 * dy0 );
-
-      area += triangleArea;
-      x += triangleArea * third * ( x0 + x1 + x2 );
-      y += triangleArea * third * ( y0 + y1 + y2 );
-    }
-
-    return {
-      x: x / area,
-      y: y / area
-    };
+    Geometry.computeCentroid( this.vertices );
   };
 
   return Polygon;

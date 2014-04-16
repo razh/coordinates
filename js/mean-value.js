9e6f868c0ee6eab2f78439c1f10e434e74ebd2b7
Raymond Zhou <somecrazyidiot@gmail.com>
2014-04-15 21:22:39 -0400

    Finish Hormann/Floater MeanValue implementation.

--- js/mean-value.js
+++ js/mean-value.js
@@ -21,15 +21,20 @@ var MeanValue = (function() {
   function _convert2d( x, y, vertices ) {
     var vertexCount = 0.5 * vertices.length;
 
+    // The edgeWeights array contains all zeros and is not used unless
+    // (x, y) lies on a vertex or edge.
+    var edgeWeights = new Array( vertexCount );
     var weights = new Array( vertexCount );
     var i;
     // Assign all zeros.
     for ( i = 0; i < vertexCount; i++ ) {
-      weights[ i++ ] = 0;
+      edgeWeights[i] = 0;
+      weights[i] = 0;
     }
 
     /**
      * Determine if (x, y) lies on a vertex or an edge.
+     * Otherwise, determine mean-value weights for (x, y).
      *
      * Subscripts:
      *   0 - Previous vertex.
@@ -48,6 +53,7 @@ var MeanValue = (function() {
      */
     var area0, area1;
     var dot0, dot1;
+    var sum = 0;
     for ( i = 0; i < vertexCount; i++ ) {
       // Current vertex.
       x1 = vertices[ 2 * i ];
@@ -66,25 +72,52 @@ var MeanValue = (function() {
       r1 = Math.sqrt( dx1 * dx1 + dy1 * dy1 );
       // (x, y) lies on (x1, y1).
       if ( !r1 ) {
-        weights[i] = 1;
-        return weights;
+        edgeWeights[i] = 1;
+        return edgeWeights;
       }
 
       area1 = 0.5 * ( dx1 * dy2 - dx2 * dy1 );
-      dot1 = ( dx1 * dx2 + dx1 * dx2 );
+      dot1 = dx1 * dx2 + dy1 * dy2;
 
       // (x, y) lies on the edge (x1, y1) - (x2, y2).
       if ( !area1 && dot1 < 0 ) {
         r2 = Math.sqrt( dx2 * dx2 + dy2 * dy2 );
-        weights[i] = r2 / ( r1 + r2 );
-        weights[ ( i + 1 ) % vertexCount ] = 1 - weights[i];
-        return weights;
+        edgeWeights[i] = r2 / ( r1 + r2 );
+        edgeWeights[ ( i + 1 ) % vertexCount ] = 1 - edgeWeights[i];
+        return edgeWeights;
       }
+
+      // Previous vertex.
+      x0 = vertices[ 2 * modulo( i - 1, vertexCount ) ];
+      y0 = vertices[ 2 * modulo( i - 1, vertexCount ) + 1 ];
+
+      dx0 = x0 - x;
+      dy0 = y0 - y;
+
+      area0 = 0.5 * ( dx0 * dy1 - dx1 * dy0 );
+      // Add contribution of first triangle.
+      if ( area0 ) {
+        r0 = Math.sqrt( dx0 * dx0 + dy0 * dy0 );
+        dot0 = dx0 * dx1 + dy0 * dy1;
+        weights[i] += ( r0 - dot0 / r1 ) / area0;
+      }
+
+      // And contribution of second triangle.
+      if ( area1 ) {
+        r2 = Math.sqrt( dx2 * dx2 + dy2 * dy2 );
+        weights[i] += ( r2 - dot1 / r1 ) / area1;
+      }
+
+      sum += weights[i];
     }
 
-    /**
-     * Determine vertex weights.
-     */
+    // Normalize weights.
+    var sumInverse = 1 / sum;
+    for ( i = 0; i < vertexCount; i++ ) {
+      weights[i] *= sumInverse;
+    }
+
+    return weights;
   }
 
   /**

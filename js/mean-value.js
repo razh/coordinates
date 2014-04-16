3d88a28bd5a98c8da68dde5d0923643a4dc4af2a
Raymond Zhou <somecrazyidiot@gmail.com>
2014-04-15 22:26:17 -0400

    Replace MeanValue implementation.
    Hormann/Floater looks to be ~4 times faster.

--- js/mean-value.js
+++ js/mean-value.js
@@ -15,10 +15,12 @@ var MeanValue = (function() {
   }
 
   /**
+   * Converts (x, y) to a set of mean-value coordinate weights.
+   *
    * Implementation of the interpolation function from K. Hormann and
    * M. S. Floater's Mean Value Coordinates for Arbitrary Planar Polygons.
    */
-  function _convert2d( x, y, vertices ) {
+  function convert2d( x, y, vertices ) {
     var vertexCount = 0.5 * vertices.length;
 
     // The edgeWeights array contains all zeros and is not used unless
@@ -114,135 +116,6 @@ var MeanValue = (function() {
     // Normalize weights.
     var sumInverse = 1 / sum;
     for ( i = 0; i < vertexCount; i++ ) {
-      weights[i] *= sumInverse;
-    }
-
-    return weights;
-  }
-
-  /**
-   * Converts (x, y) to a set of mean-value coordinate weights.
-   *
-   * Note that vertices should be arranged counter-clockwise or weights will be
-   * negative.
-   */
-  function convert2d( x, y, vertices ) {
-    var vertexCount = 0.5 * vertices.length;
-
-    var weights = [];
-    var sum = 0;
-
-    // Note that (x1, y1) and a1 represent the current vertex and angle.
-    var x0, y0, x1, y1, x2, y2;
-    var dx0, dy0, dx1, dy1, dx2, dy2;
-    var a0, a1;
-    var distance;
-    var weight;
-    var i;
-    for ( i = 0; i < vertexCount; i++ ) {
-      // Previous vertex.
-      x0 = vertices[ 2 * modulo( i - 1, vertexCount ) ];
-      y0 = vertices[ 2 * modulo( i - 1, vertexCount ) + 1 ];
-      // Current vertex.
-      x1 = vertices[ 2 * i ];
-      y1 = vertices[ 2 * i + 1 ];
-      // Next vertex.
-      x2 = vertices[ 2 * ( ( i + 1 ) % vertexCount ) ];
-      y2 = vertices[ 2 * ( ( i + 1 ) % vertexCount ) + 1 ];
-
-      // Calculate angles.
-      dx0 = x0 - x;
-      dy0 = y0 - y;
-
-      dx1 = x1 - x;
-      dy1 = y1 - y;
-
-      dx2 = x2 - x;
-      dy2 = y2 - y;
-
-      a0 = Math.atan2( dy1, dx1 ) - Math.atan2( dy0, dx0 );
-      a1 = Math.atan2( dy2, dx2 ) - Math.atan2( dy1, dx1 );
-
-      distance = Math.sqrt( dx1 * dx1 + dy1 * dy1 );
-
-      /**
-       * Calculate weight:
-       *
-       *           tan( a0 / 2 ) + tan( a1 / 2 )
-       * weight = --------------------------------
-       *                   || ai - a1 ||
-       */
-      weight = ( Math.tan( 0.5 * a0 ) + Math.tan( 0.5 * a1 ) ) / distance;
-      weights.push( weight );
-      sum += weight;
-    }
-
-    // If the sum of weights is infinite, then the point lies on either
-    // a vertex or an edge.
-    var il;
-    var i0, i1;
-    var indices;
-    var dx, dy;
-    var distanceSquared;
-    if ( !isFinite( sum ) ) {
-      sum = 0;
-      indices = [];
-      // Convert all infinite weight values to one.
-      // All other values are zeroed.
-      for ( i = 0, il = weights.length; i < il; i++ ) {
-        if ( isFinite( weights[i] ) ) {
-          weights[i] = 0;
-        } else {
-          weights[i] = 1;
-          indices.push(i);
-          sum++;
-        }
-      }
-
-      // The point is on a vertex.
-      // No need to normalize weights.
-      if ( sum === 1 ) {
-        return weights;
-      }
-
-      // The points lies on an edge.
-      if ( sum === 2 ) {
-        i0 = indices[0];
-        i1 = indices[1];
-
-        x0 = vertices[ 2 * modulo( i0, vertexCount ) ];
-        y0 = vertices[ 2 * modulo( i0, vertexCount ) + 1 ];
-        x1 = vertices[ 2 * modulo( i1, vertexCount ) ];
-        y1 = vertices[ 2 * modulo( i1, vertexCount ) + 1 ];
-
-        dx1 = x1 - x;
-        dy1 = y1 - y;
-
-        dx = x1 - x0;
-        dy = y1 - y0;
-
-        distanceSquared = dx * dx + dy * dy;
-
-        // Handle degenerate distances (two vertices have the same position).
-        if ( !distanceSquared ) {
-          weights[ i0 ] = 1;
-          weights[ i1 ] = 0;
-          return weights;
-        }
-
-        // Determine the lerp parameter with the dot product.
-        weights[ i0 ] = ( dx1 * dx + dy1 * dy ) / distanceSquared;
-        weights[ i1 ] = 1 - weights[ i0 ];
-
-        // As the combined weights must sum up to one, it is not necessary to
-        // normalize weights.
-        return weights;
-      }
-    }
-
-    // Normalize weights.
-    var sumInverse = 1 / sum;
-    for ( i = 0, il = weights.length; i < il; i++ ) {
       weights[i] *= sumInverse;
     }
 
